b444b993104f44c4208f76cee55fdd4ba9c9c579
Diego <diego.vega097@gmail.com>
2019-06-12 15:37:06 -0500

    add vista simple

--- Red Social/src/routes/index.js
+++ Red Social/src/routes/index.js
@@ -13,4 +13,7 @@ module.exports = app => {
     router.post('/images/:image_id/like', image.like);
     router.post('/images/:image_id/comment', image.comment);
     router.delete('/images/:image_id', image.remove);
+    
+    
+    app.use(router);
 }
